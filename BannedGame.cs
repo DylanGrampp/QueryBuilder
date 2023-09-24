bc7071b9dc57a711d2510f05dc9a8140fa11edd3
DylanGrampp <114005528+DylanGrampp@users.noreply.github.com>
2023-09-23 21:51:38 -0400

    Add files via upload

---
+++ BannedGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Models
+{
+    internal class BannedGame : IClassModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Series { get; set; }
+        public string Country { get; set; }
+        public string? Details { get; set; }
+
+        public BannedGame() { }
+
+        public override string ToString()
+        {
+            string msg = "";
+
+            msg += $"ID: {Id}\n";
+            msg += $"Title: {Title}\n";
+            msg += $"Series: {Series}\n";
+            msg += $"Country: {Country}\n";
+            msg += $"Details: {Details}\n";
+
+            return msg;
+        }
+    }
+}
